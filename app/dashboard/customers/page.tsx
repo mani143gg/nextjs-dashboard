905f5cf05104367e74c3071af0a5ac1680aeedb5
Manikandan G <mani@Manikandans-MacBook-Air.local>
2026-06-27 10:25:34 +0530

    Initial Commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+import Counter from '../../ui/counter'
+
+export default function Page() {
+    return (
+        <div>
+            <p> Customers page</p>
+            {/* <Counter /> */}
+        </div>
+    )
+}
